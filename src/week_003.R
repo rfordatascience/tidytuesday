9e2e0c9b3049064789c6b8afe67b0cd0857876ac
Jesse Maegan <jesse.maegan@gmail.com>
2018-05-27 11:35:48 -0500

    committed to wrong repo

--- src/week_003.R
+++
@@ -1,7 +0,0 @@
-# set up workspace 
-library(tidyverse)  # probably could load everything individually but /shrug
-library(readxl)
-library(here) 
-
-# import data
-week3 <- read_excel(here("data", "global_mortality.xlsx"))
