645816152d0ea900c3d92c2dfcb2465b78155c7f
Thomas Mock <j.thomasmock@gmail.com>
2019-09-03 09:32:24 -0500

    update data

--- data/2019/2019-09-03/moores_law.R
+++ data/2019/2019-09-03/moores_law.R
@@ -1,70 +1,3 @@
-# Moore's Law
-
-This week's data is from [Wikipedia](https://en.wikipedia.org/wiki/Transistor_count) - by way of the [Data Is Beautiful Subreddit](https://www.reddit.com/r/dataisbeautiful/comments/cynql1/moores_law_graphed_vs_real_cpus_gpus_1965_2019_oc/).
-
-Additional info and graphics can be found at [Our World in Data](https://ourworldindata.org/technological-progress).
-
-**Moore's Law: Transistors per microprocessor**
-
-* The observation that the number of transistors in a dense integrated circuit doubles approximately every two years.
-
-
-# Get the data!
-
-```
-cpu <- readr::read_csv("https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2019/2019-09-03/cpu.csv")
-
-gpu <- readr::read_csv("https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2019/2019-09-03/gpu.csv")
-
-ram <- readr::read_csv("https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2019/2019-09-03/ram.csv")
-
-```
-
-# Data Dictionary
-
-## `cpu.csv`
-
-|variable             |class     |description |
-|:--------------------|:---------|:-----------|
-|processor            |character | Processor name |
-|transistor_count     |double    | Number of transitors |
-|date_of_introduction |double    | year introduced |
-|designer             |character | Designer |
-|process              |double    | Size of manufacturing process (in nanometers)|
-|area                 |double    | Area of chip in square millimeters |
-
-## `gpu.csv`
-
-|variable             |class     |description |
-|:--------------------|:---------|:-----------|
-|processor            |character |Processor name |
-|transistor_count     |double    | Transistor count|
-|date_of_introduction |double    | year introduced|
-|designer_s           |character | designer |
-|manufacturer_s       |character | manufacturer |
-|process              |double    | size of manufacturing process (nanometers)|
-|area                 |double    | area of chip in square millimeters |
-|ref                  |character | reference |
-
-## `ram.csv`
-
-|variable             |class     |description |
-|:--------------------|:---------|:-----------|
-|chip_name            |character | Chip name |
-|capacity_bits        |character | capacity bits - essentially how many units of information it can work on, units in next column |
-|bit_units            |character | Units for bit capacity (bits < kb < Mb < Gb)|
-|ram_type             |character | Ram type |
-|transistor_count     |double    | Transistor count |
-|date_of_introduction |double    | Year introduced|
-|manufacturer_s       |character | Manufactured |
-|process              |double    | Size of manufacturing process (nanometers) |
-|area                 |double    | Area of chip in square millimeters |
-|ref                  |character | reference |
-
-
-# Cleaning Script
-
-```
 library(tidyverse)
 library(rvest)
 
@@ -156,4 +89,9 @@ write_csv(df1_clean, here::here("2019", "2019-09-03", "cpu.csv"))
 write_csv(df2_clean, here::here("2019", "2019-09-03", "gpu.csv"))
 write_csv(df3_clean, here::here("2019", "2019-09-03", "ram.csv"))
 
-```
+tomtom::create_dictionary(df1_clean)
+
+cpu <- readr::read_csv("https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2019/2019-09-03/cpu.csv")
+gpu <- readr::read_csv("https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2019/2019-09-03/gpu.csv")
+ram <- readr::read_csv("https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2019/2019-09-03/ram.csv")
+
