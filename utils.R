acbdb9e285ab676aec1c1be8749d789c6aac7bb9
kittysocks <mlmiraglia99@gmail.com>
2024-01-29 23:47:43 -0500

    added function to get most current tidytuesday data

---
+++ utils.R
@@ -0,0 +1,29 @@
+library(tidyverse)
+library(lubridate)
+
+get_current_tt_data <- function() {
+
+# Get today's date
+today_date <- Sys.Date()
+yr <- paste0(year(today_date),"/")
+
+# Find the most recent Tuesday or today if it's Tuesday
+closest_tuesday <- ifelse(lubridate::wday(today_date) >= 3,
+                          today_date + (3 - lubridate::wday(today_date)),
+                          today_date - (lubridate::wday(today_date) - 3))
+
+# Extract year and date components
+year_str <- format(as.Date(closest_tuesday), "%Y")  # Ensure closest_tuesday is a Date object
+tuesday_date_str <- format(as.Date(closest_tuesday), "%m-%d")
+data_str <- paste0(year_str,"-",tuesday_date_str)
+file_path <-paste0("data/",yr,data_str)
+
+files <- list.files(path = file_path, pattern = ".csv$", full.names = TRUE)
+
+return(files)
+}
+
+# can expand function to read files? depending on if there are multiple .csvs?
+# function to create multiple dfs of the .csv files?
+
+get_current_tt_data()
