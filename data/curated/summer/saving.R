00676293c61d0a6d815c6dd8cd743b3370f0a6b1
jonthegeek <jonthegeek@users.noreply.github.com>
2024-07-29 09:37:27 +0000

    Accept submission

--- data/curated/summer/saving.R
+++
@@ -1,6 +0,0 @@
-source("data/curated/curation_scripts.R")
-dir_name <- "summer"
-
-# Call this for each clean dataset.
-ttsave(summer_movies, dir_name = dir_name)
-ttsave(summer_movie_genres, dir_name = dir_name)
