c28627d0ebe94fa98f5cd6c2323a4b19fd50c4f7
Jon Harmon <jonthegeek@gmail.com>
2024-09-16 06:30:22 -0500

    Update data/curated/chess_game_dataset/cleaning.R

--- data/curated/chess_game_dataset/cleaning.R
+++ data/curated/chess_game_dataset/cleaning.R
@@ -3,7 +3,9 @@
 
 library(tidyverse)
 
-chess <- readr::read_csv("data/curated/chess_game_dataset/chess_games.csv") %>%
+# Data saved from Kaggle as "chess_game_dataset/chess_games.csv"
+
+chess <- readr::read_csv("chess_game_dataset/chess_games.csv") %>%
   rename("game_id" = "id",
          "start_time" = "created_at",
          "end_time" = "last_move_at",
