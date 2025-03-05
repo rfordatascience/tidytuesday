5f92a0dbc7c5565a9b60bfab776eabb954649783
frank hull <69683423+frankiethull@users.noreply.github.com>
2025-03-05 16:05:59 -0700

    Update data/curated/pokemon/cleaning.R
    Co-authored-by: Jon Harmon <jonthegeek@gmail.com>

--- data/curated/pokemon/cleaning.R
+++ data/curated/pokemon/cleaning.R
@@ -1,8 +1,3 @@
-# Paste code used to create the curated dataset here. Include comments as
-# necessary. If you did not need to clean the data, use a comment like the one
-# below, but also load the data with readr::read_csv() to ensure the data can be
-# loaded, and to use with `saving.R`. Delete this block of comments.
-
 # Clean data provided by https://github.com/williamorim/pokemon. No cleaning was necessary.
 
 # install.packages("pokemon")
