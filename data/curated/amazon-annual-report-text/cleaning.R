5595fa6bacda8a07b372a022217d3ec62e53568b
Jon Harmon <jonthegeek@gmail.com>
2025-03-23 06:55:00 -0500

    Remove url comment. Namespace `filter()`.

--- data/curated/amazon-annual-report-text/cleaning.R
+++ data/curated/amazon-annual-report-text/cleaning.R
@@ -12,7 +12,7 @@ library(tidytext)
 library(tidyverse)
 
 # Download the messy data
-url <- "https://raw.githubusercontent.com/GregoryVdvinne/gregoryvdvinne.github.io/main/Amazon_Budgets/Data/Intermediate/all_reports_ocr_uncleaned.feather"  # Replace with actual URL
+url <- "https://raw.githubusercontent.com/GregoryVdvinne/gregoryvdvinne.github.io/main/Amazon_Budgets/Data/Intermediate/all_reports_ocr_uncleaned.feather"
 destfile <- tempfile(fileext = ".feather")  # Temporary file path
 download.file(url, destfile, mode = "wb")   # Download file
 unclean_data <- read_feather(destfile)  # Read .feather file
@@ -26,7 +26,7 @@ report_words_clean <-  unclean_data |>
   # Remove stop words
   anti_join(stop_words, by = "word") |>
   # Remove some additional 'words'
-  filter(!(word %in% c(letters, LETTERS)),  # Single letters
+  dplyr::filter(!(word %in% c(letters, LETTERS)),  # Single letters
          !str_detect(word, "\\d"),          # Words containing a number
          !str_detect(word, "_"))
 
