23bff962e0f96365f885215c4194846069b8d0dd
Jon Harmon <jonthegeek@gmail.com>
2025-06-09 07:22:15 -0400

    Add cleaning.R

---
+++ data/curated/new_submission/cleaning.R
@@ -0,0 +1,9 @@
+# Clean data provided by the development version of the {historydata} R package.
+# No cleaning was necessary.
+
+# install.packages("pak")
+# pak::pak("ropensci/historydata")
+library(historydata)
+
+judges_people <- historydata::judges_people
+judges_appointments <- historydata::judges_appointments
