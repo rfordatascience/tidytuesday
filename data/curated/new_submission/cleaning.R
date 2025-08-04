25b52cc75a2535815353a77f36c39b62cb00eb8b
Jon Harmon <jonthegeek@gmail.com>
2025-08-04 06:49:36 -0500

    Add cleaning.R

---
+++ data/curated/new_submission/cleaning.R
@@ -0,0 +1,28 @@
+# Clean data *and* sample instructions for use in R provided by <source of
+# data>. Minimal cleaning applied.
+
+library(tidyverse)
+library(jsonlite)
+
+income_inequality_raw <- readr::read_csv(
+  "https://ourworldindata.org/grapher/inequality-of-incomes-before-and-after-taxes-and-transfers-scatter.csv?v=1&csvType=full&useColumnShortNames=true"
+) |>
+  dplyr::mutate(
+    Year = as.integer(Year)
+  )
+
+income_inequality_processed <- readr::read_csv(
+  "https://ourworldindata.org/grapher/gini-coefficient-before-and-after-tax-lis.csv?v=1&csvType=full&useColumnShortNames=true"
+) |>
+  dplyr::mutate(
+    Year = as.integer(Year)
+  )
+
+# Download metadata for use in construction of data dictionaries.
+metadata_raw <- jsonlite::fromJSON(
+  "https://ourworldindata.org/grapher/inequality-of-incomes-before-and-after-taxes-and-transfers-scatter.metadata.json?v=1&csvType=full&useColumnShortNames=true"
+)
+
+metadata_processed <- jsonlite::fromJSON(
+  "https://ourworldindata.org/grapher/gini-coefficient-before-and-after-tax-lis.metadata.json?v=1&csvType=full&useColumnShortNames=true"
+)
