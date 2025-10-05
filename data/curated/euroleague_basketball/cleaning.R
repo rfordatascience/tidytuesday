a41adaf3e768172e97b0a8f7240fedd34f9c2991
jonthegeek <jonthegeek@users.noreply.github.com>
2025-10-05 15:39:36 +0000

    Accept submission

--- data/curated/euroleague_basketball/cleaning.R
+++
@@ -1,7 +0,0 @@
-
-# Clean data provided by https://github.com/natanast/EuroleagueBasketball. 
-# The data was already cleaned and curated in the EuroleagueBasketball R package.
-# No cleaning was necessary.
-euroleague_basketball <- readr::read_csv("https://raw.githubusercontent.com/natanast/EuroleagueBasketball/main/data-raw/euroleague_dataset.csv")
-
-
