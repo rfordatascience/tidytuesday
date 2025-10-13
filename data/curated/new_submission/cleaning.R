ccf435978c22e9419faa8286a3ca536119debfeb
jonthegeek <jonthegeek@users.noreply.github.com>
2025-10-13 11:08:56 +0000

    Accept submission

--- data/curated/new_submission/cleaning.R
+++
@@ -1,4 +0,0 @@
-# Data obtained from Post45 Data Collective Github, no cleaning necessary
-
-prizes <- readr::read_csv("https://raw.githubusercontent.com/Post45-Data-Collective/data/refs/heads/main/british_literary_prizes/british_literary_prizes-1990-2022.csv") 
-
