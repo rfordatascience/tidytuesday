47acf2e37ec05d8b4d64673d98bb3f221f4119c8
Jon Harmon <jonthegeek@gmail.com>
2025-10-21 06:03:13 -0500

    Update data/curated/new_submission/cleaning.R

--- data/curated/new_submission/cleaning.R
+++ data/curated/new_submission/cleaning.R
@@ -9,8 +9,5 @@ library(tidyverse)
 # Load diabetes data
 diabetes = medicaldata::diabetes
 
-# Save
-write_csv(diabetes, "diabetes.csv")
-
 # No data cleaning needed
 # For more information and background on the data: https://pmc.ncbi.nlm.nih.gov/articles/PMC4418458/
