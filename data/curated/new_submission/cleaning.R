52bc41db1a0b057b34457975475b7b3f3fa9f53b
Darakhshan Nehal <darakhshannehal@gmail.com>
2025-10-21 15:55:08 +0530

    Add cleaning.R

---
+++ data/curated/new_submission/cleaning.R
@@ -0,0 +1,16 @@
+
+## Diabetes data from medicaldata package by Peter Higgins
+
+# Install medicaldata package
+#remotes::install_github("higgi13425/medicaldata")
+library(medicaldata)
+library(tidyverse)
+
+# Load diabetes data
+diabetes = medicaldata::diabetes
+
+# Save
+write_csv(diabetes, "diabetes.csv")
+
+# No data cleaning needed
+# For more information and background on the data: https://pmc.ncbi.nlm.nih.gov/articles/PMC4418458/
