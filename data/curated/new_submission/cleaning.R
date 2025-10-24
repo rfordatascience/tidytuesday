6a7066e9122e7c5891de39bb5ff26ce6dac93664
econmaett <60882754+econmaett@users.noreply.github.com>
2025-10-24 23:09:00 +0200

    Update cleaning.R

--- data/curated/new_submission/cleaning.R
+++ data/curated/new_submission/cleaning.R
@@ -40,28 +40,3 @@ climate_data <- readr::read_delim(
 ## combine datasets ----
 sechselaeuten <- dplyr::left_join(x = burn_duration, y = climate_data, by = dplyr::join_by(year)) |> 
   dplyr::mutate(record = ifelse(tre200m0 >= 19, TRUE, FALSE))
-
-## plot ----
-p <- ggplot2::ggplot(data = sechselaeuten, mapping = ggplot2::aes(x = duration, y = tre200m0)) +
-  ggplot2::geom_point(size = 1, colour = "#f7b801") +
-  ggplot2::geom_smooth(method = "lm", formula = "y ~ x", se = FALSE, colour = "#3d348b") +
-  ggplot2::geom_smooth(method = "lm", formula = "y ~ x", se = TRUE, colour = "#7678ed") +
-  ggplot2::geom_text(
-    data = dplyr::filter(sechselaeuten, record == TRUE), 
-    mapping = ggplot2::aes(x = duration, y = tre200m0, label = year), 
-    colour = "#d90429",
-    position = ggplot2::position_jitter(width = 0.5, height = 0.5)
-  ) +
-  ggplot2::scale_x_continuous(breaks = seq(0, 60, 10), limits = c(0, 60)) +
-  ggplot2::scale_y_continuous(breaks = seq(14, 22, 2), limits = c(14, 22)) +
-  ggplot2::labs(
-    title = "Can an exploding snowman predict the summer season?",
-    subtitle = "Zurich's Boeoeg as a weather oracle, 1923-2025",
-    x = "Duration until head exploded (minutes)", 
-    y = "Average summer temperature (°C)"
-  ) +
-  ggplot2::theme_bw(base_size = 6)
-
-print(p)  
-
-ggplot2::ggsave(filename = "sechselaeuten.png", plot = p, path = "tt_submission/", width = 10, height = 6, units = "cm", bg = "white")
