1cc5af13beff3fd6b05e6728370ca30f36b67260
Erin Grand <eringrand@gmail.com>
2025-12-23 15:34:14 -0500

    Add cleaning.R

---
+++ data/curated/new_submission/cleaning.R
@@ -0,0 +1,11 @@
+# remotes::install_github("eringrand/astropic")
+
+# Dataset inside the the {{astropic}} R package on GitHub.
+library(astropic)
+library(dplyr)
+data("hist_apod")
+
+# Remove one column with constant values
+apod <- hist_apod
+dataset <- apod |> 
+  select(-service_version)
