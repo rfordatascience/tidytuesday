aa376548424f218b319273e4e3406574a5794a57
Jon Harmon <jonthegeek@gmail.com>
2026-01-31 08:17:33 -0600

    Genericize the script by providing download instructions and a reproducible download location.
    Replace smart quotes with normal quotes (I don't know that this matters but it doesn't hurt for machine-readibility, and I'm PRETTY sure we've had at least one case where it caused weirdness).

--- data/curated/edible-plants/cleaning.R
+++ data/curated/edible-plants/cleaning.R
@@ -1,7 +1,13 @@
 library(tidyverse)
 library(RODBC)
 
-raw_data <- odbcConnectAccess2007("tt_submission//plant1.accdb")
+# Source https://discovery.dundee.ac.uk/en/datasets/edible-plant-database/
+# does not allow automatic downloads, so the data was downloaded locally. Here
+# we assume the data is saved in your `tempdir()`.
+
+download_dir <- tempdir()
+data_path <- file.path(download_dir, "plant1.accdb")
+raw_data <- odbcConnectAccess2007(data_path)
 plants <- sqlFetch(raw_data, "Edible plants")
 edible_plants <- plants |>
   as_tibble() |>
@@ -32,14 +38,23 @@ edible_plants <- plants |>
     across(where(is.character), ~ na_if(.x, "Currently no data available."))
   ) |>
   mutate(soil = str_trim(soil)) |>
-  mutate(energy = if_else(
-    is.na(nutritional_info) & energy == 0, NA, energy
-  )) |>
-  separate(preferred_ph, into = c("preferred_ph_lower", "preferred_ph_upper"), sep = "-|–", fill = "right") |>
+  mutate(
+    energy = if_else(
+      is.na(nutritional_info) & energy == 0,
+      NA,
+      energy
+    )
+  ) |>
+  separate(
+    preferred_ph,
+    into = c("preferred_ph_lower", "preferred_ph_upper"),
+    sep = "-|–",
+    fill = "right"
+  ) |>
   mutate(
     across(starts_with("preferred"), as.numeric)
   ) |>
   mutate(temperature_growing = str_remove_all(temperature_growing, " ")) |>
-  mutate(across(where(is.character), ~str_squish(.x)))
+  mutate(across(where(is.character), ~ str_squish(.x)))
 
 odbcCloseAll()
