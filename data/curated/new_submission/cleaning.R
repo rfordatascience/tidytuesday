105e38d63b6bdd60f56caf2bb57bf0b7deda1491
Cormac Monaghan <89480646+C-Monaghan@users.noreply.github.com>
2026-02-06 12:17:32 +0000

    Add cleaning.R

---
+++ data/curated/new_submission/cleaning.R
@@ -0,0 +1,42 @@
+################################################################################
+# Science Foundation Ireland (SFI) Grants Commitments
+# https://www.sfi.ie/about-us/governance/open-data/Open-Data-2024-07-31.csv
+# Accessed 2026-02-05
+################################################################################
+
+# Packages ---------------------------------------------------------------------
+library(readr)
+library(dplyr)
+library(stringr)
+
+# Loading and tidying dataset --------------------------------------------------
+sfi_grants <- readr::read_csv(
+  "https://www.sfi.ie/about-us/governance/open-data/Open-Data-2024-07-31.csv"
+) |>
+  janitor::clean_names() |>
+  dplyr::select(
+    start_date,
+    end_date,
+    proposal_id,
+    programme_name,
+    sub_programme,
+    supplement,
+    research_body,
+    research_body_ror_id,
+    funder_name,
+    crossref_funder_registry_id,
+    proposal_title,
+    current_total_commitment
+  ) |>
+  # 1. Change dates from character format to date format
+  # 2. current_total_commitment is currently as character but for some values has
+  #   a comma and or () in it. We will remove these before converting to a numeric.
+  dplyr::mutate(
+    start_date = as.Date(start_date),
+    end_date = as.Date(end_date),
+    current_total_commitment = stringr::str_remove_all(
+      current_total_commitment,
+      "\\(|,|\\)"
+    ),
+    current_total_commitment = as.double(current_total_commitment)
+  )
