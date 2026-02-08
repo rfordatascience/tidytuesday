36f3d38cae5ec4cfcb6d3183a54953db096255ea
Jon Harmon <jonthegeek@gmail.com>
2026-02-08 15:19:17 -0600

    Delete extra cleaning script.

--- data/curated/olympics-2026-events/cleaning.R
+++
@@ -1,3 +0,0 @@
-# Clean data provided by @chendaniely. No cleaning was necessary.
-dataset_url <- "https://raw.githubusercontent.com/chendaniely/olympics-2026/refs/heads/main/data/final/olympics/olympics_events.csv"
-schedule <- readr::read_csv(dataset_url)
