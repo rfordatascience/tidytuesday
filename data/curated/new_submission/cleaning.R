d7f3eed8ffdd8c1e197e6369fea418979dd86286
Jon Harmon <jonthegeek@gmail.com>
2026-03-08 08:55:35 -0500

    Tweaks before acceptance
    `cleaning.R`:
    
    - Made it slightly clearer how to manually get your path ready for the script.
    - Added `library()` calls at the top to help people see what they need to install.

--- data/curated/new_submission/cleaning.R
+++ data/curated/new_submission/cleaning.R
@@ -1,12 +1,22 @@
-# Data downloaded from https://dashboard.repairmonitor.org/?language=en, cleaning to fix variable names and data types
+# Data downloaded from https://dashboard.repairmonitor.org/?language=en as
+# "repairs-en.xlsx". Cleaning applied to fix variable names and data types.
 
-repairs <- readxl::read_xlsx("repairs-en.xlsx") %>%
+library(tidyverse)
+library(janitor)
+
+# Update this to point to your downloaded file.
+repairs_xlsx_file <- "repairs-en.xlsx"
+
+repairs <- readxl::read_xlsx(repairs_xlsx_file) %>%
   janitor::clean_names() %>%
-  dplyr::mutate(repair_date = ymd(repair_date), 
-         repair_cafe_number = as.integer(repair_cafe_number), 
-         estimated_year_of_production = as.integer(estimated_year_of_production),   
-         if_not_repaired_why_could_you_not_repair_it_open_answer = as.character(if_not_repaired_why_could_you_not_repair_it_open_answer),
-         reparability_of_product_1_difficult_10_easy = as.integer(reparability_of_product_1_difficult_10_easy))
-         
-         
-
+  dplyr::mutate(
+    repair_date = ymd(repair_date),
+    repair_cafe_number = as.integer(repair_cafe_number),
+    estimated_year_of_production = as.integer(estimated_year_of_production),
+    if_not_repaired_why_could_you_not_repair_it_open_answer = as.character(
+      if_not_repaired_why_could_you_not_repair_it_open_answer
+    ),
+    reparability_of_product_1_difficult_10_easy = as.integer(
+      reparability_of_product_1_difficult_10_easy
+    )
+  )
