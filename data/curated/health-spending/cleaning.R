9c194f77fb4797239df02c41db797ab3b87cb51a
Jon Harmon <jonthegeek@gmail.com>
2026-03-20 08:53:32 -0500

    Delete data/curated/health-spending/cleaning.R
    We only need one of cleaning.py or cleaning.R. The purpose of the file is to show people how the data was curated.

--- data/curated/health-spending/cleaning.R
+++
@@ -1,7 +0,0 @@
-# The cleaning for this dataset was done in Python. See cleaning.py.
-# Data from the WHO Global Health Expenditure Database (GHED)
-# To regenerate the CSVs: pip install -r requirements.txt && python cleaning.py
-
-health_spending <- readr::read_csv("health_spending.csv")
-financing_schemes <- readr::read_csv("financing_schemes.csv")
-spending_purpose <- readr::read_csv("spending_purpose.csv")
