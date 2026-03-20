6e61f32f24c66f5e12a37cfd5152471872c89b53
Luca Picci <lpicci96@gmail.com>
2026-03-20 11:58:04 +0100

    Add R script for loading health spending datasets

---
+++ data/curated/health-financing/cleaning.R
@@ -0,0 +1,7 @@
+# The cleaning for this dataset was done in Python. See cleaning.py.
+# Data from the WHO Global Health Expenditure Database (GHED)
+# via the bblocks-data-importers package (pip install bblocks-data-importers)
+
+health_spending <- readr::read_csv("health_spending.csv")
+financing_schemes <- readr::read_csv("financing_schemes.csv")
+spending_purpose <- readr::read_csv("spending_purpose.csv")
