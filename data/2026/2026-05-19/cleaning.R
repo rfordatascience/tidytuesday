6aa21e02bfbd5828e009dd4a19194744dbb95bec
Alexandre Bédard-Vallée <74552873+bedard-valleea@users.noreply.github.com>
2026-05-18 06:51:29 -0400

    Crossref state of metadata (#1029)
    * initial version of data for submission
    
    * update datasets to latest version
    
    * Update data/curated/crossref-state-of-metadata/meta.yaml
    
    * Accept submission
    
    * Delete data/2026/2026-05-19/instructions.md
    
    I could have sworn the assign script would auto-delete this, since it isn't needed at this stage. I'm writing myself a ticket to fix that!
    
    * Credit bedard-valleea, too!
    
    ---------
    
    Co-authored-by: Alex Bédard-Vallée <abedard-vallee@crossref.org>
    Co-authored-by: Jon Harmon <jonthegeek@gmail.com>
    Co-authored-by: jonthegeek <jonthegeek@users.noreply.github.com>

---
+++ data/2026/2026-05-19/cleaning.R
@@ -0,0 +1,4 @@
+# Clean data provided by Crossref. No cleaning was necessary.
+metadata_coverage_stats_by_country <- readr::read_csv("https://zenodo.org/api/records/19928426/files/metadata_coverage_stats_by_country.csv/content")
+member_participation_stats_by_country <- readr::read_csv("https://zenodo.org/api/records/19928426/files/member_participation_stats_by_country.csv/content")
+
