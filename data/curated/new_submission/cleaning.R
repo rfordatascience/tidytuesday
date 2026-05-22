e67388622a26b06facb745a03b6ec28f06409beb
Nicola Rennie <42338476+nrennie@users.noreply.github.com>
2026-05-22 15:03:31 +0100

    Update cleaning.R

--- data/curated/new_submission/cleaning.R
+++ data/curated/new_submission/cleaning.R
@@ -1,8 +1,13 @@
 library(tidyverse)
 library(readxl)
 
-# Download XLSX file from https://zenodo.org/records/17648712
-raw_data <- read_xlsx("EPLP/EPLP_Dataset_Workbook_v2.xlsx", sheet = 2, skip = 1)
+# If you don't want to automatically download unknown files, download the XLSX file from https://zenodo.org/records/17648712
+xlsx_url <- "https://zenodo.org/records/17648712/files/EPLP_Dataset_Workbook_v2.xlsx?download=1"
+xlsx_file <- "EPLP_Dataset_Workbook_v2.xlsx"
+if (!file.exists(xlsx_file)) {
+  download.file(xlsx_url, destfile = xlsx_file, mode = "wb")
+}
+raw_data <- read_xlsx(xlsx_file, sheet = 2, skip = 1)
 
 eplp <- raw_data |>
   # Missing
