c0277b5026d8b553b09953be0740e66febcf929a
Jon Harmon <jonthegeek@gmail.com>
2026-06-28 08:45:07 -0500

    Clean up cleaning.R
    - Remove instructions.
    - Show how to install fightr.
    - Fix the filter to run without error (not sure if it's *right*, but it doesn't throw an error, at least).

--- data/curated/new_submission/cleaning.R
+++ data/curated/new_submission/cleaning.R
@@ -1,16 +1,15 @@
-# Paste code used to create the curated dataset here. Include comments as
-# necessary. If you did not need to clean the data, use a comment like the one
-# below, but also load the data with readr::read_csv() to ensure the data can be
-# loaded, and to use with `saving.R`. Delete this block of comments.
-
-# Clean data provided by <source of data>. No cleaning was necessary.
+# install.packages("pak")
+# pak::pak("benyamindsmith/fightr")
 
 library(fightr)
+# fightr::update_all_ufc_data()
 library(dplyr)
+library(lubridate)
+
 ufc_athletes <- fightr::get_ufc_data("ufc_athletes")
 
 # Removing Events that do not have results
-ufc_fights <- fightr::get_ufc_data("ufc_fights")|>
+ufc_fights <- fightr::get_ufc_data("ufc_fights") |>
   dplyr::filter(!is.na(f1_result))
 
 ufcstats_data <- fightr::get_ufc_data("ufcstats_data")
