03397b004863ec88b832c43f814f096ee32f0a04
Jon Harmon <jonthegeek@gmail.com>
2026-07-05 14:40:05 -0500

    Resave datasets with clean column names

--- data/curated/new_submission/cleaning.R
+++ data/curated/new_submission/cleaning.R
@@ -1,18 +1,23 @@
-# Paste code used to create the curated dataset here. Include comments as
-# necessary. If you did not need to clean the data, use a comment like the one
-# below, but also load the data with readr::read_csv() to ensure the data can be
-# loaded, and to use with `saving.R`. Delete this block of comments.
-
-# Clean data provided by <source of data>. No cleaning was necessary.
+# install.packages("pak")
+pak::pak("benyamindsmith/fightr")
 
 library(fightr)
 library(dplyr)
-ufc_athletes <- fightr::get_ufc_data("ufc_athletes")
+library(janitor)
+
+fightr::update_all_ufc_data()
+
+ufc_athletes <- fightr::get_ufc_data("ufc_athletes") |> 
+  janitor::clean_names()
 
 # Removing Events that do not have results
 ufc_fights <- fightr::get_ufc_data("ufc_fights")|>
-  dplyr::filter(!is.na(f1_result))
+  dplyr::filter(!is.na(f1_result)) |> 
+    janitor::clean_names()
 
-ufcstats_data <- fightr::get_ufc_data("ufcstats_data")
-ultimate_ufc_dataset<- fightr::get_ufc_data("ultimate_ufc_dataset")
-ufc_rankings_dataset <- fightr::get_ufc_data("ufc_rankings_dataset")
+ufcstats_data <- fightr::get_ufc_data("ufcstats_data") |> 
+  janitor::clean_names()
+ultimate_ufc_dataset<- fightr::get_ufc_data("ultimate_ufc_dataset") |> 
+  janitor::clean_names()
+ufc_rankings_dataset <- fightr::get_ufc_data("ufc_rankings_dataset") |> 
+  janitor::clean_names()
